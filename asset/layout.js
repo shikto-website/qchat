1b6d568d53dbe05c6f99e560488d408e5a1781f9
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-02-25 22:46:18 +0600

    Add files via upload

--- asset/layout.js
+++ asset/layout.js
@@ -18,7 +18,35 @@ var layout = {
                     textSize:18
                 })
             ])
-        ])
+        ]),
+        (()=>{
+            if(Query().pwa){
+                return Container()
+            }else{
+                return (
+                    Button({text:"", id:"installAppButton", backgroundColor:Colors.white},[
+                        Row([
+                            Container({
+                                size:[50,50],
+                                backgroundColor:Colors.white
+                            },[                
+                                Image({
+                                    src:"asset/image/favicon.png",
+                                    height:"fill"
+                                })
+                            ]),
+                            Container({size:[10,20]}),
+                            Text({
+                                text:"Install App",
+                                textStyle:"bold",
+                                textSize:18,
+                                textColor:Colors.black
+                            })
+                        ])
+                    ])
+                )
+            }
+        })()
     ],
 
     chatRoomScreen:[
